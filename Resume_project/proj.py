8b639a91f0ee7825ec7993bb2f99dfc534dbf2be
jasonwong567 <114112753+jasonwong567@users.noreply.github.com>
2024-12-19 12:31:10 -0500

    Add files via upload

---
+++ Resume_project/proj.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="The One Piece", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I am Jason")
+st.title("Welcome to My Webpage")
+st.write("This is a test application.")
+st.write("hellow")
+
+st.markdown("[Go to Vacuum Chamber](Vacuumchamber)")
+
+
+
+
